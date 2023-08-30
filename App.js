f5f089529e47a92672b7f8f3e55fc676868c8944
jorgeecamps <jorgeecamps@gmail.com>
2023-08-29 22:40:24 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Botao/>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function Botao (props) {
+  return( 
+  <Text>Seja bem vindo</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
